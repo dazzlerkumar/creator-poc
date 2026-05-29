58f22fdd572739dcaf9567063691d68ffc315ddc
dazzlerkumar <dazzler.deepak98@gmail.com>
2026-05-30 02:18:22 +0530

    refactor(payment-success-card): update ui

--- src/components/payments/payment-success-card.tsx
+++ src/components/payments/payment-success-card.tsx
@@ -1,18 +1,29 @@
-import { CheckCircle2 } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { CheckCircle2, X } from "lucide-react";
 import { useUIStore } from "@/stores/ui-store";
 import { OverlayCardWrapper } from "./overlay-card-wrapper";
-import { HABUILD_WEBSITE_URL } from "@/lib/constants";
+import { useState, useEffect } from "react";
 
 export const PaymentSuccessCard = () => {
   const { setShowPayment } = useUIStore();
-  const handleViewBenefits = () => {
-    window.open(HABUILD_WEBSITE_URL, "_blank", "noopener noreferrer");
-  };
+  const [showClose, setShowClose] = useState(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowClose(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <OverlayCardWrapper className="bg-accent-foreground border-accent p-5 py-6 justify-between">
-      <div className="mb-6">
+      {showClose && (
+        <button
+          onClick={() => setShowPayment(false)}
+          className="absolute top-4 right-5 transition-opacity"
+        >
+          <X className="w-6 h-6 text-red-400" />
+        </button>
+      )}
+      <div className="mb-6 mt-2">
         <div className="flex items-center gap-2 mb-2">
           <CheckCircle2 className="w-7 h-7 text-accent-foreground fill-accent" />
           <h3 className="text-foreground text-base font-bold leading-tight">
@@ -22,22 +33,6 @@ export const PaymentSuccessCard = () => {
         <p className="text-grey-2 text-sm font-medium leading-[1.5]">
           Welcome to the community! Your daily wellness journey begins now.
         </p>
-      </div>
-      <div className="flex flex-col gap-2">
-        <Button
-          onClick={handleViewBenefits}
-          variant={"default"}
-          className="h-12 bg-accent text-white rounded-4xl"
-        >
-          View Benefits
-        </Button>
-        <Button
-          onClick={() => setShowPayment(false)}
-          variant={"outline"}
-          className="h-12 bg-transparent border border-accent/20 text-accent hover:bg-accent/10 rounded-4xl"
-        >
-          Hide
-        </Button>
       </div>
     </OverlayCardWrapper>
   );
